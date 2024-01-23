a5117bbaacd1994ea71d5f51ed0fb47d2a561dcf
ludoviclenoir <ludoviclenoir1@gmail.com>
2024-01-23 18:46:49 +0100

    extract zip with python

---
+++ scripts/extract_zip.py
@@ -0,0 +1,14 @@
+# extract zip into folder
+
+import os
+import zipfile
+
+local_zip = '/tmp/rps.zip'
+zip_ref = zipfile.ZipFile(local_zip, 'r')
+zip_ref.extractall('/tmp/')
+zip_ref.close()
+
+local_zip = '/tmp/rps-test-set.zip'
+zip_ref = zipfile.ZipFile(local_zip, 'r')
+zip_ref.extractall('/tmp/')
+zip_ref.close()

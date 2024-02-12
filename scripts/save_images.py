71e65c645a7a733f0ce68062f8860510fdf68930
ludoviclenoir <ludoviclenoir1@gmail.com>
2024-02-12 12:43:19 +0100

    try with predefined model

--- scripts/save_images.py
+++ scripts/save_images.py
@@ -7,10 +7,13 @@ import logging
 import time
 import datetime
 
-
 #tensorflow
 import tensorflow as tf
 from keras_preprocessing.image import ImageDataGenerator
+
+#Mobilnet
+from tensorflow.keras.applications import MobileNetV2
+from tensorflow.keras.layers import Dense, GlobalAveragePooling2D
 
 #convert tensorflow h5 to .json
 import subprocess
@@ -78,22 +81,47 @@ def save_image():
         logging.error(str(e))
         return "Une erreur s'est produite", 500
 
+# Fonction pour charger et adapter le modèle MobileNetV2
+def load_pretrained_model(num_classes):
+    # Chargement du modèle MobileNetV2 pré-entraîné sans les couches fully-connected
+    base_model = MobileNetV2(weights='imagenet', include_top=False, input_shape=(224, 224, 3))
+    
+    # Congelez les couches du modèle pré-entraîné pour empêcher l'entraînement
+    for layer in base_model.layers:
+        layer.trainable = False
+    
+    # Ajoutez des couches fully-connected personnalisées adaptées à votre nombre de classes
+    x = base_model.output
+    x = GlobalAveragePooling2D()(x)  # Ajoutez une couche de pooling
+    x = Dense(512, activation='relu')(x)  # Ajoutez une couche fully-connected
+    predictions = Dense(num_classes, activation='softmax')(x)  # Couche de sortie avec softmax pour la classification
+    
+    # Créez le modèle final en spécifiant les entrées et les sorties
+    model = tf.keras.Model(inputs=base_model.input, outputs=predictions)
+    
+    return model
+
 # Vos opérations de génération de graphiques
-def generate_plots():
-    epochs = range(1, 11)
-    acc = np.random.rand(10)
-    loss = np.random.rand(10)
+def generate_plots(history):
+    acc = history.history['accuracy']
+    loss = history.history['loss']
+    epochs = range(1, len(acc) + 1)
 
-    # Tracer les courbes d'entraînement
     plt.plot(epochs, acc, 'r', label='Training accuracy')
-    plt.plot(epochs, loss, 'b', label='Training loss')
+    plt.title('Training accuracy')
     plt.xlabel('Epochs')
-    plt.ylabel('Accuracy / Loss')
+    plt.ylabel('Accuracy')
     plt.legend()
-    plt.title('Training Metrics')
 
-    # Enregistrer le graphique dans un fichier
-    plt.savefig('training_metrics.png')
+    plt.figure()
+
+    plt.plot(epochs, loss, 'b', label='Training loss')
+    plt.title('Training loss')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.legend()
+
+    plt.show()
 
 @app.route('/generate-model', methods=['POST'])
 def generate_model():
@@ -105,78 +133,29 @@ def generate_model():
         new_width = largest_dimension
         new_height = int(largest_dimension * aspect_ratio)
 
-        print("new dimension")
-        print(new_width, new_height)  # Affiche les nouvelles dimensions
-
-
         # Remplacez ceci par le chemin du répertoire de données
         TRAINING_DIR = "/home/ludovic/Documents/sites/api-recognition/training_data"
 
-        #nom du model pour l'export
         name_model = "/home/ludovic/Documents/sites/api-recognition/training_data/book.h5"
 
         # Chemin pour enregistrer le fichier JSON des classes
         classes_json_path = "/home/ludovic/Documents/sites/api-recognition/book/modele/classes.json"
 
+        # Obtenez une liste de tous les éléments dans le répertoire
+        all_items = os.listdir(TRAINING_DIR)
 
-        logging.debug("Début de la section ImageDataGenerator")
-        # Utilisez ImageDataGenerator pour prétraiter les images
-        training_datagen = ImageDataGenerator(
-            rescale=1./255,
-            rotation_range=40, # Rotation aléatoire jusqu'à 40 degrés
-            width_shift_range=0.2, # Décalage horizontal aléatoire
-            height_shift_range=0.2, # Décalage vertical aléatoire
-            shear_range=0.2, # Effet de cisaillement
-            zoom_range=0.2, # Zoom aléatoire
-            horizontal_flip=True, # Retournement horizontal aléatoire
-            fill_mode='nearest') # Mode de remplissage pour les pixels ajoutés
+        # Filtrez les dossiers uniquement
+        folders = [item for item in all_items if os.path.isdir(os.path.join(TRAINING_DIR, item))]
+        num_classes = len(folders)
 
-        logging.debug("Début de la section flow_from_directory")
+        logging.debug("nombres de dossier:")
+        logging.debug(num_classes)
 
-        # Chargement des données d'entraînement
-        train_generator = training_datagen.flow_from_directory(
-            TRAINING_DIR,
-            target_size=(new_width, new_height),  # Utilisez les dimensions calculées
-            class_mode='categorical',
-            batch_size=64)
+        # logging.debug("nombres de dossier:")
+        # logging.debug(folders)
 
-        # Nombre de catégories (à ajuster en fonction du nombre de sous répertoire)
-        nombre_de_categories = len(train_generator.class_indices)
-
-        print("Nombre de catégories :", nombre_de_categories)
-
-        #calcul automatique de steps per epoch
-        steps_per_epoch = max(1, nombre_de_categories // train_generator.batch_size)
-
-        #steps_per_epoch = 25
-
-        logging.debug("Début de la section Construction du modèle")
-
-        # Construction du modèle
-        model = tf.keras.models.Sequential([
-            # Première couche de convolution
-            tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(new_width, new_height, 3)),
-            tf.keras.layers.MaxPooling2D(2, 2),
-            #deuxième couche    
-            tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
-            tf.keras.layers.MaxPooling2D(2,2),
-            # The third convolution
-            tf.keras.layers.Conv2D(128, (3,3), activation='relu'),
-            tf.keras.layers.MaxPooling2D(2,2),
-
-
-            # The fourth convolution
-            # tf.keras.layers.Conv2D(128, (3,3), activation='relu'),
-            # tf.keras.layers.MaxPooling2D(2,2),
-
-            # Aplatir la sortie pour la connecter à des couches denses
-            tf.keras.layers.Flatten(),
-            # Couche dense, vous pouvez ajuster le nombre de neurones
-            tf.keras.layers.Dense(256, activation='relu'),
-
-            # Dernière couche dense, le nombre de neurones doit correspondre au nombre de classes
-            tf.keras.layers.Dense(nombre_de_categories, activation='softmax')
-        ])
+        # Chargement du modèle pré-entraîné
+        model = load_pretrained_model(num_classes)
 
         # Configurer TensorBoard pour surveiller les métriques d'entraînement
         tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
@@ -185,64 +164,32 @@ def generate_model():
 
         model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
 
-        logging.debug("Entraînement du modèle")
+        # Utilisez ImageDataGenerator pour prétraiter les images
+        training_datagen = ImageDataGenerator(
+            rescale=1./255,
+            rotation_range=40,
+            width_shift_range=0.2,
+            height_shift_range=0.2,
+            shear_range=0.2,
+            zoom_range=0.2,
+            horizontal_flip=True,
+            fill_mode='nearest')
+
+        # Chargement des données d'entraînement
+        train_generator = training_datagen.flow_from_directory(
+            TRAINING_DIR,
+            target_size=(new_width, new_height),
+            class_mode='categorical',
+            batch_size=32)
 
         # Entraînement du modèle
-        history = model.fit(train_generator, epochs=25, steps_per_epoch=steps_per_epoch, verbose=1, callbacks=[tensorboard_callback])
+        history = model.fit(train_generator, epochs=25, verbose=1, callbacks=[tensorboard_callback])
 
-        logging.debug("history")
-
-        logging.debug(history)
-
-        logging.debug("Sauvegarde du modèle")
+        # Générer et afficher les graphiques d'entraînement
+        generate_plots(history)
 
         # Sauvegarde du modèle
         model.save(name_model)
-
-        logging.debug("destination du fichier:")
-
-        logging.debug(name_model)
-
-        # Visualisation de la précision et de la perte pendant l'entraînement
-        generate_plots()
-
-        #test accuracy TODO A DEBUG
-
-        # import matplotlib.pyplot as plt
-        # acc = history.history['accuracy']
-        # val_acc = history.history['val_accuracy']
-        # loss = history.history['loss']
-        # val_loss = history.history['val_loss']
-
-        # epochs = range(len(acc))
-
-        # plt.plot(epochs, acc, 'r', label='Training accuracy')
-        # plt.plot(epochs, val_acc, 'b', label='Validation accuracy')
-        # plt.title('Training and validation accuracy')
-        # plt.legend(loc=0)
-        # plt.figure()
-
-        # plt.show()
-
-        # import numpy as np
-        # from google.colab import files
-        # from keras.preprocessing import image
-
-        # uploaded = files.upload()
-
-        # for fn in uploaded.keys():
-        
-        #     # predicting images
-        #     path = fn
-        #     img = image.load_img(path, target_size=(150, 150))
-        #     x = image.img_to_array(img)
-        #     x = np.expand_dims(x, axis=0)
-
-        #     images = np.vstack([x])
-        #     classes = model.predict(images, batch_size=10)
-        #     print(fn)
-        #     print(classes)
-
         #generation des classes
 
         # Obtenir la liste des noms des sous-répertoires (classes)
